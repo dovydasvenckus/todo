bb16e8df6f77f1f77d68bbb7eec4071d924b883b
Dovydas Venckus <dovydas.venckus@live.com>
2017-01-25 22:00:26 +0200

    Added guava for reducing utils code

--- src/main/java/com/dovydasvenckus/todo/auth/BasicAuthHeaderDecoder.java
+++ src/main/java/com/dovydasvenckus/todo/auth/BasicAuthHeaderDecoder.java
@@ -4,26 +4,24 @@ import com.dovydasvenckus.todo.helper.auth.UsernamePasswordPair;
 
 import java.util.Base64;
 
-import static com.dovydasvenckus.todo.util.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.base.Strings.nullToEmpty;
 import static java.util.Arrays.stream;
 
 public class BasicAuthHeaderDecoder {
     private static Base64.Decoder decoder = Base64.getDecoder();
 
     public static UsernamePasswordPair decode(String authHeader) {
-        checkArgument(isStringNonEmpty(authHeader), "Auth header should not be empty or null");
+        authHeader = nullToEmpty(authHeader).trim();
+        checkArgument(!isNullOrEmpty(authHeader), "Auth header should not be empty or null");
         checkArgument(isValidAuthHeaderStructure(authHeader), "Header should start with 'Basic' keyword");
-        authHeader = authHeader.trim();
 
         String decodedString = new String(decoder.decode(authHeader.split(" ")[1]));
         checkArgument(isValidUsernamePassword(decodedString), "Username and password should be separated by colon");
         return new UsernamePasswordPair(
                 decodedString.split(":")[0],
                 decodedString.split(":")[1]);
-    }
-
-    private static boolean isStringNonEmpty(String string) {
-        return string != null && !string.trim().isEmpty();
     }
 
     private static boolean isValidAuthHeaderStructure(String header) {
