9077658bbdd565d0af75eab22f0558f1313829b9
Dovydas Venckus <dovydas.venckus@live.com>
2017-02-04 23:29:56 +0200

    Added logging loaded database driver

--- src/main/java/com/dovydasvenckus/todo/helper/db/connector/DatabaseConnector.java
+++ src/main/java/com/dovydasvenckus/todo/helper/db/connector/DatabaseConnector.java
@@ -1,18 +1,23 @@
 package com.dovydasvenckus.todo.helper.db.connector;
 
 import com.dovydasvenckus.todo.helper.db.DatabaseConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sql2o.Sql2o;
 
 public abstract class DatabaseConnector {
-    protected final String driverClassName;
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConnector.class);
+
+    private final String driverClassName;
 
     DatabaseConnector(String driverClassName) {
         this.driverClassName = driverClassName;
+        logger.info("Loading {} database driver", driverClassName);
     }
 
     public abstract Sql2o getInstance(DatabaseConfig databaseConfig);
 
-    protected void loadDriver() throws ClassNotFoundException {
+    void loadDriver() throws ClassNotFoundException {
         Class.forName(driverClassName);
     }
 }
